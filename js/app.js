e5421c035dd5296f8a71a0366a75f9de8c12086f
Steven <Steven.otieno1@gmail.com>
2018-05-30 22:39:04 +0300

    Add js file

---
+++ js/app.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+// function myFunction() {
+//    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+</head>
+
+<body>
+
+// If JavaScript in Head
+
+<p id="demo">A Paragraph.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+<script>
+// function myFunction() {
+//    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+
+// If Function is stored in an external file called "myScript.js"
+
+<script src="myScript.js"></script>
+
+// If Function is stored in an external file called "myScript.js"
+
+<script src="/js/myScript.js"></script>
+
+// If Function is stored in an external file called "myScript.js"
+
+<script src="https://URL/js/myScript.js"></script>
+
+// If Function is stored in an external file called "myScript.js"
+
+<script src="/js/myScript.js"></script>
+
+
+</body>
+</html>
